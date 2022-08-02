4e04be1f06836cc76381ed934e19a76fc2a45201
Dmitry Filippov <demodima97@mail.ru>
2022-08-03 02:34:19 +0300

    шрифты, цвета, настройка окружения

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>всем привет</h1>
+      <h2>в этом чятике</h2>
+      <p>
+        кто че <span>делает</span> го по фотке
+      </p>
+    </div>
+  );
+}
+
+export default App;
